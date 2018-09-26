58268f33948c26ba6faa5e995152345bb64cee5c
夏江楠 <mokar@xiajiangnandeiMac.local>
2018-09-26 11:37:41 +0800

    加入判断手机设备以及浏览器类型

--- index.js
+++ index.js
@@ -1059,61 +1059,78 @@ GetLocalTime.prototype = {
 }
 var GetLocalTime = new GetLocalTime();
 
-// export {
-//     idDom,
-//     classDom,
-//     tagDom,
-//     QSDom,
-//     QSADom,
-//     createDom,
-//     createtxt,
-//     addDom,
-//     addBody,
-//     isEmpty,
-//     lTrim,
-//     trim,
-//     rTrim,
-//     isNumber,
-//     customEvent,
-//     getParmeter,
-//     bubbleSort,
-//     descendingSort,
-//     getArrMax,
-//     getArrMaxVal,
-//     unique,
-//     rnd,
-//     ajax,
-//     cookie,
-//     isOnline,
-//     lStore,
-//     sStore,
-//     goPage,
-//     htmlFontSize,
-//     isPhone,
-//     isWeixin,
-//     bind,
-//     unbind,
-//     dataType,
-//     soleString32,
-//     findNum,
-//     getPos,
-//     normalDate,
-//     dateFormat0,
-//     dateFormat1,
-//     changeTwoDecimal_f,
-//     getDecimal,
-//     myAjax,
-//     getScrollTop,
-//     getScrollHeight,
-//     getWindowHeight,
-//     getBottom,
-//     goTop,
-//     ScrollTextLeft,
-//     ScrollTextTop,
-//     arrRandom，
-//     deepCopy,
-//     Check,
-//     getCurrentTime,
-//     getOneweekDate,
-//     getBeforeMonth
-// }
+//判断手机设备以及浏览器类型
+function isPhoneSys(){
+  var u = navigator.userAgent, app = navigator.appVersion;
+  return {         //移动终端浏览器版本信息
+    trident: u.indexOf('Trident') > -1, //IE内核
+    presto: u.indexOf('Presto') > -1, //opera内核
+    webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+    gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+    mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+    ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+    android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+    iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+    iPad: u.indexOf('iPad') > -1, //是否iPad
+    webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+  };
+}
+export {
+    idDom,
+    classDom,
+    tagDom,
+    QSDom,
+    QSADom,
+    createDom,
+    createtxt,
+    addDom,
+    addBody,
+    isEmpty,
+    lTrim,
+    trim,
+    rTrim,
+    isNumber,
+    customEvent,
+    getParmeter,
+    bubbleSort,
+    descendingSort,
+    getArrMax,
+    getArrMaxVal,
+    unique,
+    rnd,
+    ajax,
+    cookie,
+    isOnline,
+    lStore,
+    sStore,
+    goPage,
+    htmlFontSize,
+    isPhone,
+    isWeixin,
+    bind,
+    unbind,
+    dataType,
+    soleString32,
+    findNum,
+    getPos,
+    normalDate,
+    dateFormat0,
+    dateFormat1,
+    changeTwoDecimal_f,
+    getDecimal,
+    myAjax,
+    getScrollTop,
+    getScrollHeight,
+    getWindowHeight,
+    getBottom,
+    goTop,
+    ScrollTextLeft,
+    ScrollTextTop,
+    arrRandom，
+    deepCopy,
+    Check,
+    getCurrentTime,
+    getOneweekDate,
+    getBeforeMonth,
+    isPhoneSys
+}
